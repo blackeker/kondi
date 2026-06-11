595fd22dee7ec45b66e56e2a381d8cef61bbf28e
blackeker <blackeker@gmail.com>
2026-06-11 17:02:25 +0300

    güncelleme

--- app/src/main/java/com/myanim/kondi/data/hdfilmcehennemi/HdFilmCehennemiScraper.kt
+++ app/src/main/java/com/myanim/kondi/data/hdfilmcehennemi/HdFilmCehennemiScraper.kt
@@ -19,7 +19,7 @@ import kotlin.math.pow
 
 class HdFilmCehennemiScraper {
     companion object {
-        private val client = OkHttpClient.Builder()
+        private val client = com.myanim.kondi.util.NetworkUtils.getUnsafeOkHttpClientBuilder()
             .connectTimeout(20, TimeUnit.SECONDS)
             .readTimeout(20, TimeUnit.SECONDS)
             .writeTimeout(20, TimeUnit.SECONDS)
